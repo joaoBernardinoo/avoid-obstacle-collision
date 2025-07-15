e14bed3d9aba35992438372944cc6566619ba28c
joaoBernardinoo <joaobernardino@ufba.br>
2025-07-14 23:28:39 -0300

    refactor: Simplify data collection and add debugging
    This commit refactors the `collectDataHDF5` function to remove the unused `dist` and `lidar_data` parameters, streamlining the data collection process. Additionally, debugging `print` statements have been added to provide visibility into the saving of camera images and angles.

--- controllers/robot/data_collection.py
+++ controllers/robot/data_collection.py
@@ -1,40 +1,26 @@
 import numpy as np
 import h5py
-import os
 from typing import cast
 from constants import SAVE_PATH, HDF5_SAVE_PATH
 
 
-def collectDataHDF5(dist, angle, lidar_data, camera_data, cam_w, cam_h):
+def collectDataHDF5(angle, camera_data, cam_w, cam_h):
     image_np = np.frombuffer(camera_data, np.uint8).reshape((cam_h, cam_w, 4))
-    lidar_np = np.array(lidar_data)
 
     with h5py.File(HDF5_SAVE_PATH, 'a') as hf:
         if 'camera_image' not in hf:
             hf.create_dataset('camera_image', data=[image_np],
                               compression="gzip", chunks=True,
                               maxshape=(None, cam_h, cam_w, 4))
-            hf.create_dataset('lidar_data', data=[lidar_np],
-                              compression="gzip", chunks=True,
-                              maxshape=(None, len(lidar_np)))
-            hf.create_dataset('dist', data=[dist],
-                              compression="gzip", chunks=True,
-                              maxshape=(None,))
             hf.create_dataset('angle', data=[angle],
                               compression="gzip", chunks=True,
                               maxshape=(None,))
         else:
+            print("Salvando imagem...")
+            print(f"Salvando angulo {angle * 180 / np.pi}")
             camera_dset = cast(h5py.Dataset, hf['camera_image'])
             camera_dset.resize((camera_dset.shape[0] + 1), axis=0)
             camera_dset[-1] = image_np
-
-            lidar_dset = cast(h5py.Dataset, hf['lidar_data'])
-            lidar_dset.resize((lidar_dset.shape[0] + 1), axis=0)
-            lidar_dset[-1] = lidar_np
-
-            dist_dset = cast(h5py.Dataset, hf['dist'])
-            dist_dset.resize((dist_dset.shape[0] + 1), axis=0)
-            dist_dset[-1] = dist
 
             angle_dset = cast(h5py.Dataset, hf['angle'])
             angle_dset.resize((angle_dset.shape[0] + 1), axis=0)
